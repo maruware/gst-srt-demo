073ecbc14592b2260b774e55ac15c19733855e83
maruware <me@maruware.com>
2020-05-29 18:34:13 +0900

    add audio

--- producer.sh
+++ producer.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 gst-launch-1.0 -v \
+  mpegtsmux name=mux \
   videotestsrc is-live=true \
-! video/x-raw,width=640,height=360,framerate=30/1 \
-! x264enc tune=zerolatency speed-preset=superfast key-int-max=60 \
-! mpegtsmux \
-! srtserversink  uri="srt://:7002" latency=500
+  ! video/x-raw,width=640,height=360,framerate=30/1 \
+  ! x264enc tune=zerolatency speed-preset=superfast key-int-max=60 \
+  ! queue \
+  ! mux. \
+  audiotestsrc is-live=true wave=8 \
+  ! audioconvert \
+  ! voaacenc bitrate=64000 \
+  ! queue \
+  ! mux. \
+  mux. \
+  ! srtserversink  uri="srt://:7002" latency=500
